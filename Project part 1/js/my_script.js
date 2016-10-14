3d51332cd8dd6efc428f283556a62341a4c47e4b
mattstra <mattrules128@gmail.com>
2016-10-13 20:58:19 -0400

    Finished part 1

--- Project part 1/js/my_script.js
+++ Project part 1/js/my_script.js
@@ -4,10 +4,16 @@ function submit_form() {
 
   for(var i=0; i < radios.length; i++)
   {
-    if(radios[i].checked)
+    if(radios[0].checked)
     {
       alert("You are now leaving this website");
       window.open("http://chautauquawatershed.org/index.php/bigfoot");
+      return false;
+    }
+
+    else {
+      alert("Bigfoot needs to find a good home");
+      return false;
     }
   }
 }
